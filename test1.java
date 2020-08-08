d42f305bbe2345761aa71a99999669c853227de9
Abdul <java.nabi@gmail.com>
2020-08-08 22:06:14 +0530

    Issue-1111

--- test1.java
+++ test1.java
@@ -13,6 +13,8 @@ public class test   {
 		
 		
 		
+		
+		
         // input from standard input - keyboard
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter a number: ");
