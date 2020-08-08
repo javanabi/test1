e8b2f03bfd47aa8cfa07f3b04ca4c47852397b88
Abdul <java.nabi@gmail.com>
2020-08-08 22:09:31 +0530

    Issue-1111

--- test.java
+++ test.java
@@ -6,13 +6,6 @@ public class test   {
         // Creates a reader instance which takes
 		
 		
-	
-		
-		
-		
-		
-		
-		
 		
         // input from standard input - keyboard
         Scanner reader = new Scanner(System.in);
