754bbcb8f3efd7172b4cdeca6814526cbb645627
javanabi <java.nabi@gmail.com>
2020-08-17 16:49:10 +0530

    Issue-1111

--- test.java
+++ test.java
@@ -5,7 +5,7 @@ public class test   {
         // Creates a reader instance which takes
 		
 		 
-        // input from standard input - keyboard
+        // input fro m standard input - keyboard
         Scanner reader = new Scanner(System.in);
 		 
 		
--- test1.java
+++ test1.java
@@ -13,7 +13,7 @@ public class test   {
         // nextInt() reads the next integer from the keyboard
         int number = reader.nextInt();
 
-         // println() prints the following line to the output screen
+          // println() prints the following line to the output screen
 		
 		
 		
