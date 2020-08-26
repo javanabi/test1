4d94a72c6827d9b09a648e2944a64234ee764afb
AbdulNabi01 <abbu.nabi@gmail.com>
2020-08-26 16:13:46 +0530

    test commit

--- test1.java
+++ test1.java
@@ -14,11 +14,7 @@ public class test   {
 
             // println() prints the following line to the output screen
 		
-		
-		
-		
-		
-		
+		 
 		
 		
         System.out.println("You entered: " + number);
