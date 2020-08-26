e00caaa245c45be6f8343c125b2530d61dbac8d5
javanabi <java.nabi@gmail.com>
2020-08-26 09:42:15 +0530

    Issue - 12345

--- test1.java
+++ test1.java
@@ -2,6 +2,7 @@ public class test   {
 
     public static void main(String[] args) {
 
+
         // Creates a reader instance which takes
 		// input from standard input - keyboard
 		               
