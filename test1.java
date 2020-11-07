00fadb91df4565b98067c5ecf2455b93f5011979
Abdul <java.nabi@gmail.com>
2020-11-07 11:52:10 +0530

    sfasdfasdf

--- test1.java
+++ test1.java
@@ -6,7 +6,7 @@ public class test   {
 		// input from standard input - keyboard
 		             
 		 Scanner reader = new Scanner(System.in);
-		 System.out.println("test");  
+		 System.out.println("test");    
 		 
 		 
 		 
